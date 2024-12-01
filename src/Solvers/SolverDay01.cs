3813602fd2b81511ec1a9c40bb571d5ed43f4bf4
Anton Sizikov <asizikov@github.com>
2024-12-01 10:12:31 +0100

    🎄 Day 1, pt 1

--- src/Solvers/SolverDay01.cs
+++ src/Solvers/SolverDay01.cs
@@ -3,7 +3,20 @@ namespace aoc_2024.Solvers;
 public class SolverDay01 : ISolver {
     [PuzzleInput("01-01")]
     public void Solve(string[] input) {
+        var (first, second) = (new PriorityQueue<int, int>(), new PriorityQueue<int, int>());
+        foreach (var line in input) {
+            var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var (a, b) = (int.Parse(parts[0]), int.Parse(parts[1]));
+            first.Enqueue(a, -a);
+            second.Enqueue(b, -b);
+        }
+
         var answer = 0;
+        while (first.Count > 0) {
+            var (a, b) = (first.Dequeue(), second.Dequeue());
+            answer += Math.Abs(a - b);
+        }
+
         Console.WriteLine($"Answer: {answer}");
     }
 }
