12cdae07b926fe1b883debd4d643c36f2220e64d
Anton Sizikov <asizikov@github.com>
2024-12-01 10:24:24 +0100

    🎅🏻 prepare for day two

---
+++ src/Solvers/SolverDay02.cs
@@ -0,0 +1,10 @@
+namespace aoc_2024.Solvers;
+
+public class SolverDay02 : ISolver {
+    [PuzzleInput("02-01")]
+    public void Solve(string[] input) {
+
+        var answer = 0;
+        Console.WriteLine($"Answer: {answer}");
+    }
+}
