a3765c8e92bf6fd5db5b17a68240300cf59e0d3d
Anton Sizikov <asizikov@github.com>
2024-12-02 07:37:25 +0100

    🎄 day two; p1

--- src/Solvers/SolverDay02.cs
+++ src/Solvers/SolverDay02.cs
@@ -3,8 +3,36 @@ namespace aoc_2024.Solvers;
 public class SolverDay02 : ISolver {
     [PuzzleInput("02-01")]
     public void Solve(string[] input) {
-
         var answer = 0;
+        foreach (var line in input) {
+            var levels = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var isUnsafe = false;
+            var isIncreasing = levels[0] < levels[1];
+            for (var i = 0; i < levels.Length - 1; i++) {
+                if (levels[i] == levels[i + 1]) {
+                    isUnsafe = true;
+                    break;
+                }
+                if (isIncreasing && levels[i] > levels[i + 1]) {
+                    isUnsafe = true;
+                    break;
+                }
+                if (!isIncreasing && levels[i] < levels[i + 1]) {
+                    isUnsafe = true;
+                    break;
+                }
+                if (Math.Abs(levels[i] - levels[i + 1]) > 3) {
+                    isUnsafe = true;
+                    break;
+                }
+                
+            }
+            if (!isUnsafe) {
+                answer++;
+            }
+        }
+
+
         Console.WriteLine($"Answer: {answer}");
     }
 }
