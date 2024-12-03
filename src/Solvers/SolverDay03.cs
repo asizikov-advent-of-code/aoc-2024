b4f497478712aeacac8dca51c3fbee0f3b352bf1
Anton Sizikov <asizikov@github.com>
2024-12-03 11:47:07 +0100

    ☃️ day 3, part 2

--- src/Solvers/SolverDay03.cs
+++ src/Solvers/SolverDay03.cs
@@ -7,23 +7,42 @@ public class SolverDay03 : ISolver {
     [PuzzleInput("03-01")]
     public void Solve(string[] input) {
         var answer = 0L;
+        var enabled = true;
         foreach (var inputLine in input) {
-            answer += ParseLine(inputLine);
+            
+            var ( sum, state) = ParseLine(inputLine, enabled);
+            answer += sum;
+            enabled = state;
         }
 
         Console.WriteLine($"Answer: {answer}");
     }
 
-    long ParseLine(string inputLine) {
-        var regex = new Regex(@"mul\((\d+),(\d+)\)");
+    (long sum, bool enabled) ParseLine(string inputLine, bool enabled) {
+        var regex = new Regex(@"mul\((\d+),(\d+)\)|do\(\)|don't\(\)");
         var matches = regex.Matches(inputLine);
         var result = 0L;
+        var multiply = enabled;
         foreach (Match match in matches) {
-            var x = int.Parse(match.Groups[1].Value);
-            var y = int.Parse(match.Groups[2].Value);
-            result += x * y;
+            switch (match.Groups[0].Value)
+            {
+                case "do()":
+                    multiply = true;
+                    break;
+                case "don't()":
+                    multiply = false;
+                    break;
+                default: {
+                    if (multiply) {
+                        var x = long.Parse(match.Groups[1].Value);
+                        var y = long.Parse(match.Groups[2].Value);
+                        result += x * y;
+                    }
+                    break;
+                }
+            }
         }
 
-        return result;
+        return (result, multiply);
     }
 }
