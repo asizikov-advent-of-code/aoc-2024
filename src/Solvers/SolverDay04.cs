b2664492e99dd30ccb56a3441e2d57367d5b1880
Anton Sizikov <asizikov@github.com>
2024-12-04 10:34:42 +0100

    🏝️ Day four, part 2

--- src/Solvers/SolverDay04.cs
+++ src/Solvers/SolverDay04.cs
@@ -3,32 +3,39 @@ using System.Text;
 namespace aoc_2024.Solvers;
 
 public class SolverDay04 : ISolver {
-    [PuzzleInput("04-00")]
+    [PuzzleInput("04-01")]
     public void Solve(string[] input) {
-        var xmas = "XMAS";
-        (int dr, int dc)[] dirs = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1) ];
+        var xmas = "MAS";
+        (int dr, int dc)[] dirs = [(1, 1), (-1, 1), (-1, -1), (1, -1)];
         var answer = 0;
+        
         for (var r = 0; r < input.Length; r++) {
             for (var c = 0; c < input[r].Length; c++) {
-                if (input[r][c] != 'X') continue;
-                foreach (var dir in dirs) {
-                    Visit(r, c, 0, dir);
-                }
+                if (input[r][c] != 'A') continue;
+                Check(r, c);
             }
         }
-        
+
         Console.WriteLine($"Answer: {answer}");
 
 
-        void Visit(int r, int c, int i, (int dr, int dc) dir) {
-            if (r < 0 || r >= input.Length || c < 0 || c >= input[r].Length || input[r][c] != xmas[i]) {
-                return;
+        void Check(int r, int c) {
+            var found = 0;
+            foreach (var dir in dirs) {
+                (int r, int c) pos1 = (r + dir.dr, c + dir.dc);
+                (int r, int c) pos2 = (r - dir.dr, c - dir.dc);
+
+                if (pos1.r < 0 || pos1.r >= input.Length || pos1.c < 0 || pos1.c >= input[pos1.r].Length) continue;
+                if (pos2.r < 0 || pos2.r >= input.Length || pos2.c < 0 || pos2.c >= input[pos2.r].Length) continue;
+
+                if (input[pos1.r][pos1.c] == xmas[0] && input[pos2.r][pos2.c] == xmas[2]) {
+                    found++;
+                }
             }
-            if (i == xmas.Length - 1) {
+
+            if (found >= 2) {
                 answer++;
-                return;
-            } 
-            Visit(r + dir.dr, c + dir.dc, i + 1, dir);
+            }
         }
     }
 }
