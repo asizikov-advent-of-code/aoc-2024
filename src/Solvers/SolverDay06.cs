f770e0fad37b3e6a9db1a5b560334756aa38d4c4
Anton Sizikov <asizikov@github.com>
2024-12-06 11:24:01 +0100

    ☃️ day 6 part 2

--- src/Solvers/SolverDay06.cs
+++ src/Solvers/SolverDay06.cs
@@ -7,9 +7,8 @@ public class SolverDay06 : ISolver {
     public void Solve(string[] input) {
         var grid = input.Select(x => x.ToCharArray()).ToArray();
         var path = new HashSet<(int r, int c)>();
-        var positions = new HashSet<((int r, int c)pos, (int dr, int dc) dir)>();
-        var obstacles = new HashSet<(int r, int c)>();
-        var start = (0, 0);
+
+        (int r, int c) start = (0, 0);
         for (var r = 0; r < input.Length; r++) {
             for (var c = 0; c < input[r].Length; c++) {
                 if (input[r][c] != '^') continue;
@@ -18,51 +17,47 @@ public class SolverDay06 : ISolver {
             }
         }
 
-        obstacles.Remove(start);
-        foreach (var row in grid) {
-            Console.WriteLine(row);
+        var obstacles = new HashSet<(int r, int c)>();
+        var positions = new HashSet<(int r, int c, int dr, int dc)>();
+        (int r, int c) obstacle = (-1, -1);
+        foreach (var pos in path) {
+            obstacle = pos;
+            positions.Clear();
+            if (IsLoop(start.r, start.c, (-1, 0))) obstacles.Add(obstacle);
         }
+
         
         Console.WriteLine($"Answer: {path.Count}, obstacles: {obstacles.Count}");
 
+        bool IsLoop(int r, int c, (int dr, int dc) dir) {
+            if (!positions.Add((r, c, dir.dr, dir.dc))) return true;
+            (int r, int c) nextPos = (r + dir.dr, c + dir.dc);
+            if (nextPos.r < 0 || nextPos.r >= input.Length || nextPos.c < 0 || nextPos.c >= input[nextPos.r].Length) {
+                return false;
+            }
+
+            if (input[nextPos.r][nextPos.c] == '#' || obstacle == nextPos) {
+                var nextDir = (dir.dc, -dir.dr);
+                return IsLoop(r, c, nextDir);
+            }
+            return IsLoop(nextPos.r, nextPos.c, dir);
+        }
 
         void Walk(int r, int c, (int dr, int dc) dir) {
-            positions.Add(((r, c), dir));
             path.Add((r, c));
-            if (grid[r][c] == '.') grid[r][c] = 'X';
-            
             (int r, int c) nextPos = (r + dir.dr, c + dir.dc);
-            var nextDir = dir;
             var nextDirRotated = (dir.dc, -dir.dr);
             (int r, int c) nextPosRotated = (r + dir.dc, c - dir.dr);
-            
+
             if (nextPos.r < 0 || nextPos.r >= input.Length || nextPos.c < 0 || nextPos.c >= input[nextPos.r].Length) {
                 return;
             }
-            
-            if (Trace(nextPosRotated.r, nextPosRotated.c, nextDirRotated)) {
-                if (grid[nextPos.r][nextPos.c] == 'X') {
-                    grid[nextPos.r][nextPos.c] = '*';
-                } else {
-                    grid[nextPos.r][nextPos.c] = 'O';
-                }
-                
-                if (start != nextPos) obstacles.Add(nextPos);
-            }
-            
-            if (input[nextPos.r][nextPos.c] == '#') Walk(nextPosRotated.r, nextPosRotated.c, nextDirRotated);
-            else Walk(nextPos.r, nextPos.c, nextDir);
 
-            bool Trace(int r, int c, (int dr, int dc) dir) {
-                while (r >= 0 && r < input.Length && c >= 0 && c < input[r].Length) {
-                    if (input[r][c] == '#') return false;
-                    if (positions.Contains(((r, c), dir))) return true;
-                    r += dir.dr;
-                    c += dir.dc;
-                }
-
-                return false;
+            if (input[nextPos.r][nextPos.c] == '#') {
+                var nextDir = (dir.dc, -dir.dr);
+                Walk(nextPosRotated.r, nextPosRotated.c, nextDirRotated);
             }
+            else Walk(nextPos.r, nextPos.c, dir);
         }
     }
 }
