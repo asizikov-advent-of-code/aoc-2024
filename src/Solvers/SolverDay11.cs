d49f43cbfa785aed85b49834860b1c1c9fe9724d
Anton Sizikov <asizikov@github.com>
2024-12-11 15:29:53 +0100

    🎁 day 11 pt 2 🌰

--- src/Solvers/SolverDay11.cs
+++ src/Solvers/SolverDay11.cs
@@ -1,30 +1,36 @@
 namespace aoc_2024.Solvers;
 
 public class SolverDay11 : ISolver {
+    
     [PuzzleInput("11-01")]
     public void Solve(string[] input) {
-        
         var stones = input[0].Split(" ").ToList();
-        for (var i = 0; i < 25; i++) {
-            var next = new List<string>();
-            foreach(var stone in stones) {
-                if(stone is "0") next.Add("1");
-                else if (stone.Length % 2 == 0) {
-                    var half = stone.Length / 2;
-                    next.Add(stone.Substring(0, half));
-                    var secondHalf = stone.Substring(half);
-                    secondHalf = secondHalf.TrimStart('0');
-                    next.Add(secondHalf is "" ? "0" : secondHalf);
-                }
-                else {
-                    var val = long.Parse(stone);
-                    next.Add((val * 2024).ToString());
-                }
-            }
-            stones = next;
-        }
-        
-        var answer = stones.Count;
+        var cache = new Dictionary<(int, string), long>();
+        var answer = stones.Sum(stone => Build(75, stone));
         Console.WriteLine($"Answer: {answer}");
+        
+        long Build(int iteration, string stone) {
+            if (cache.ContainsKey((iteration, stone))) return cache[(iteration, stone)];
+            if (iteration is 0) { return 1; }
+
+            long res;
+            if (stone is "0") {
+                res = Build(iteration - 1, "1");
+            } else if (stone.Length % 2 == 0) {
+                var half = stone.Length / 2;
+                var firstHalf = stone.Substring(0, half);
+                var secondHalf = stone.Substring(half);
+                secondHalf = secondHalf.TrimStart('0');
+                secondHalf = secondHalf is "" ? "0" : secondHalf;
+
+                res = Build(iteration - 1, firstHalf) + Build(iteration - 1, secondHalf);
+            } else {
+                var val = long.Parse(stone);
+                res = Build(iteration - 1, (val * 2024).ToString());
+            }
+
+            cache[(iteration, stone)] = res;
+            return res;
+        }
     }
 }
