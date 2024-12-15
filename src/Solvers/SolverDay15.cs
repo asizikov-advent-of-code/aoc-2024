37bcf7f8e08cd0a77d5da9e0a8889571506cf4e2
Anton Sizikov <asizikov@github.com>
2024-12-15 16:07:22 +0100

    ☃️ day 15 pt 2

--- src/Solvers/SolverDay15.cs
+++ src/Solvers/SolverDay15.cs
@@ -3,85 +3,109 @@ using aoc_2024.Utils;
 
 namespace aoc_2024.Solvers;
 
-public class SolverDay15 :ISolver {
+public class SolverDay15 : ISolver {
     [PuzzleInput("15-01")]
     public void Solve(string[] input) {
-        var gridWidth = 0;
+        var visualize = false;
         var grid = new List<char[]>();
         var movements = new StringBuilder();
         var gridComplete = false;
-        for (var i = 0; i < input.Length; i++) {
-            var line = input[i];
+        foreach (var line in input) {
             if (line == string.Empty) {
                 gridComplete = true;
                 continue;
             }
 
             if (!gridComplete) {
-                grid.Add(line.ToCharArray());
+                var lineBuilder = new StringBuilder();
+                foreach (var tile in line) {
+                    lineBuilder.Append(tile switch {
+                        '#' => "##",
+                        '@' => "@.",
+                        'O' => "[]",
+                        _ => ".."
+                    });
+                }
+                grid.Add(lineBuilder.ToString().ToCharArray());
             }
-            else {
-                movements.Append(line);
-            }
+            else movements.Append(line);
         }
-        
-        var instructions = movements.ToString();
-        foreach(var (r, c, _) in grid.ScanFor('@')) {
-            Start(r, c);
-        }
-        
-        foreach (var row in grid) {
-            Console.WriteLine(new string(row));
-        }
-        Console.WriteLine();
 
-        var answer = 0;
-        foreach (var box in grid.ScanFor('O')) {
-            answer += 100* box.r + box.c;
-        }
+        Print();
+        var instructions = movements.ToString();
+        var robotStart = grid.ScanFor('@').First();
+        Start(robotStart.r, robotStart.c);
+
+        var answer = grid.ScanFor('[').Aggregate(0L, (acc, pos) => acc + (pos.r * 100 + pos.c));
         
         Console.WriteLine("answer: " + answer);
-        
-        
+
+
         void Start(int r, int c) {
             (int r, int c) robotPos = (r, c);
-            foreach (var instruction in instructions) {
-                grid[robotPos.r][robotPos.c] = '.';
-                switch (instruction) {
-                    case '^': robotPos = TryMove(robotPos, (-1, 0)); break;
-                    case 'v': robotPos = TryMove(robotPos, (1, 0)); break;
-                    case '<': robotPos = TryMove(robotPos, (0, -1)); break;
-                    case '>': robotPos = TryMove(robotPos, (0, 1)); break;
-                    default: continue;
+            foreach (var (i,instruction) in instructions.Index()) {
+                if (visualize) {
+                    Console.Clear();
+                    Console.WriteLine($"Instruction: {instruction}, {i} out of {instructions.Length}");    
                 }
-                grid[robotPos.r][robotPos.c] = '@';
+                
+                var rollback = CloneGrid();
+                (int dr, int dc) dir = instruction switch {
+                    '^' => (-1, 0),
+                    'v' => (1, 0),
+                    '<' => (0, -1),
+                    '>' => (0, 1),
+                    _ => (0, 0)
+                };
+                
+                if (TryMove(robotPos, dir))
+                    robotPos = (robotPos.r + dir.dr, robotPos.c + dir.dc);
+                else grid = rollback;
+
+                if (visualize) {
+                    Print();
+                    Thread.Sleep(100);    
+                }
+                
             }
+        }
+
+        void Print() {
+            foreach (var row in grid) {
+                Console.WriteLine(new string(row));
+            }
+
+            Console.WriteLine();
+        }
+
+        List<char[]> CloneGrid() {
+            var clone = new List<char[]>();
+            foreach (var row in grid) {
+                clone.Add(row.Clone() as char[]);
+            }
+            return clone;
         }
         
-        (int r, int c) TryMove((int r, int c) robot, (int dr, int dc) dir) {
-            (int r, int c) newPos = (robot.r + dir.dr, robot.c + dir.dc);
-            if (grid[newPos.r][newPos.c] is '.') return newPos;
-            if (grid[newPos.r][newPos.c] is '#') return robot;
-            if (grid[newPos.r][newPos.c] is 'O') {
-                var (canMove, pos) = Trace(newPos, (dir.dr, dir.dc));
-                if (!canMove) return robot;
-                grid[pos.r][pos.c] = 'O';
-                grid[newPos.r][newPos.c] = '.';
-                return newPos;
+        bool TryMove((int r, int c) p, (int dr, int dc) dir)
+        {
+            (int r, int c) next = (p.r + dir.dr, p.c + dir.dc);
+            switch (grid[next.r][next.c])
+            {
+                case '#':
+                    return false;
+                case '[':
+                    if (TryMove((next.r, next.c + 1), dir) && TryMove( next, dir)) return TryMove(p, dir);
+                    return false;
+                case ']':
+                    if (TryMove((next.r,  next.c - 1), dir) && TryMove( next, dir)) return TryMove( p, dir);
+                    return false;
+                case '.':                  
+                    grid[next.r][ next.c] = grid[p.r][p.c];
+                    grid[p.r][ p.c] = '.';                   
+                    return true;
             }
-            return robot;
-        }
-        
-        (bool canMove, (int r, int c) pos) Trace((int r, int c) start, (int dr, int dc) dir) {
-            (int r, int c) current = start;
-            while (true) {
-                current = (current.r + dir.dr, current.c + dir.dc);
-                if (grid[current.r][current.c] is '#') return (false, default);
-                if (grid[current.r][current.c] is 'O') continue;
-                if (grid[current.r][current.c] is '.') return (true, current);
-            }
-        }
+
+            return false;
+        }   
     }
-
-
 }
