e733e517582ed08b977f1d52eedd10842f867f29
Anton Sizikov <asizikov@github.com>
2024-12-16 15:33:46 +0100

    🥶 day 16 pt 2

--- src/Solvers/SolverDay16.cs
+++ src/Solvers/SolverDay16.cs
@@ -6,59 +6,36 @@ public class SolverDay16 : ISolver {
     [PuzzleInput("16-01")]
     public void Solve(string[] input) {
         var (r, c, __) = input.ScanFor('S').First();
-        var grid = input.Select(l => l.ToCharArray()).ToArray();
         var minScore = int.MaxValue;
-        var minPath = new List<(int r, int c, char d)>();
-        Walk2((r, c), (0, 1));
-
-        Console.WriteLine($"Minscore {minScore}");
-
-        void Print(List<(int r, int c, char d)> path) {
-            for (var r = 0; r < grid.Length; r++) {
-                for (var c = 0; c < grid[r].Length; c++) {
-                    if (path.Any(p => p.r == r && p.c == c)) {
-                        Console.Write(path.First(p => p.r == r && p.c == c).d);
-                    } 
-                    else {
-                        Console.Write(grid[r][c]);
-                    }
-                }
-                Console.WriteLine();
-            }
-        }
+        var minPath = new HashSet<(int r, int c)>();
+        Walk((r, c), (0, 1));
         
-        void Walk2((int r, int c) pos, (int dr, int dc) dir) {
-            var stack = new Stack<((int r, int c)pos, (int dr, int dc) dir, int score, List<(int r, int c, char d)> path)>();
+        Console.WriteLine($"Minscore {minScore}, path length: {minPath.Count}");
+        
+        
+        void Walk((int r, int c) pos, (int dr, int dc) dir) {
+            var stack = new Stack<((int r, int c)pos, (int dr, int dc) dir, int score, List<(int r, int c)> path)>();
             stack.Push((pos, dir, 0, new()));
             var visited = new Dictionary<((int r, int c)p, (int dr, int dc) d), int>();
             while (stack.Any()) {
                 var ((r, c), (dr, dc), score, path) = stack.Pop();
                 if (visited.ContainsKey(((r, c), (dr, dc)))) {
-                    if (visited[((r, c), (dr, dc))] <= score) continue;
+                    if (visited[((r, c), (dr, dc))] < score) continue;
                     visited[((r, c), (dr, dc))] = score;
                 } else {
                     visited.Add(((r, c), (dr, dc)), score);
                 }
 
-                var ch = (dr, dc) switch {
-                    (0, 1) => '>',
-                    (0, -1) => '<',
-                    (1, 0) => 'v',
-                    (-1, 0) => '^',
-                };
-                path.Add(((r, c, ch)));
+                path.Add((r, c));
 
-                if (score > minScore) continue;
+                if (score > 160624) continue;
                 if (input[r][c] == 'E') {
-                    if (minScore > score) {
-                        Console.WriteLine($"New min score: {score}");
-                        minPath = path;
-                        Console.Clear();
-                        Print(path);
-                        Console.WriteLine($"Score: {score} ; MinScore: {minScore}");
-                        Thread.Sleep(100);
-                    }
                     minScore = Math.Min(minScore, score);
+                    if (minScore == 160624) {
+                        foreach (var p in path) {
+                            minPath.Add((p.r, p.c));
+                        }
+                    }
                     continue;
                 }
 
@@ -70,20 +47,14 @@ public class SolverDay16 : ISolver {
                 for (var i = 0; i < dirs.Length; i++) {
                     (int r, int c) next = (r + dirs[i].dr, c + dirs[i].dc);
                     if (input.IsInBounds(next) && input[next.r][next.c] != '#' && input[next.r][next.c] != 'S') {
-                        var newPath = new List<(int r, int c, char d)>(path);
+                        var newPath = new List<(int r, int c)>(path);
                         if (i != 0) {
                             newPath.RemoveAt(newPath.Count - 1);
-                            newPath.Add((r, c, (dirs[i].dr, dirs[i].dc) switch {
-                                (0, 1) => '>',
-                                (0, -1) => '<',
-                                (1, 0) => 'v',
-                                (-1, 0) => '^',
-                            }));
+                            newPath.Add((r, c));
                         }
                         stack.Push((next, (dirs[i].dr,  dirs[i].dc), i == 0 ? score + 1 : score + 1001, newPath));
                     }
                 }
-                grid[r][c] = '.';
             }
         }
     }
