ddeb6f3cbc332628d96f5a2b3c53c780b8dcf4c5
Anton Sizikov <asizikov@github.com>
2024-12-20 17:03:46 +0100

    ❄️ day 20 pt 1

--- src/Solvers/SolverDay20.cs
+++ src/Solvers/SolverDay20.cs
@@ -5,82 +5,50 @@ namespace aoc_2024.Solvers;
 public class SolverDay20 : ISolver {
     [PuzzleInput("20-01")]
     public void Solve(string[] input) {
-        var path = new List<(int r, int c)>();
-        foreach (var start in input.ScanFor('S')) {
-            path= Run((start.r, start.c));
-        }
-        Console.WriteLine(path.Count - 1);
-        
-        var allPaths = new List<int>();
-        allPaths = RunWithCheats(path.First());
-        
-        allPaths.GroupBy(p => path.Count - 1 - p)
-            .OrderBy(g => g.Key)
-            .ToList().ForEach(g => Console.WriteLine($"{g.Count()} cheats saved {g.Key}  "));
-        
-        Console.WriteLine(allPaths.Select(p => path.Count-1 - p >= 100).Count());
 
-        List<int> RunWithCheats((int r, int c) start) {
-            var saves = new Dictionary<((int r, int c)s, (int r, int c)e), int>();
-            var pathLengths = new List<int>();
-            var stack = new Stack<((int r, int c) p, HashSet<(int r, int c)> visited, int steps, List<(int r, int c)> cheats)>();
-            stack.Push((start, new(), 0, new()));
+        var start = input.ScanFor('S').First();
+        var path= Run((start.r, start.c));
+        Console.WriteLine(path.Count);
 
-            while (stack.Count > 0) {
-                var current = stack.Pop();
-                if (!current.visited.Add(current.p)) continue;
-                if (current.cheats.Count == 2 && input[current.p.r][current.p.c] == '#') continue;
-                if (current.steps >= path.Count - 1) continue;
-                if (input[current.p.r][current.p.c] == 'E') {
-                    if (current.steps >= path.Count - 1) continue;
-                    saves.TryAdd((current.cheats[0], current.cheats[1]), int.MaxValue);
-                    saves[(current.cheats[0], current.cheats[1])] = Math.Min(saves[(current.cheats[0], current.cheats[1])], current.steps);
-                    Console.WriteLine("Cheats: " + string.Join(", ", current.cheats) + " saved: " + (path.Count - current.steps - 1));
-                    pathLengths.Add(current.steps);
-                    continue;
+        var answer = 0;
+        var saves = new Dictionary<int, HashSet<((int r, int ), (int, int))>>();
+        for (var s = 0; s < path.Count-1; s++) {
+            for (var e = s + 1; e < path.Count; e++) {
+                var dist = Math.Abs(path[s].r - path[e].r) + Math.Abs(path[s].c - path[e].c);
+                if (dist == 2) {
+                    var savedDist = e - (s + dist) ;
+                    if (savedDist == 0) continue;
+                    saves.TryAdd(savedDist, new HashSet<((int, int ), (int, int))>());
+                    saves[savedDist].Add((path[s], path[e]));
+                    if (savedDist >= 100) answer++;    
                 }
-                foreach ((int dr, int dc) dir in new[] { (0, 1), (0, -1), (1, 0), (-1, 0) }) {
-                    (int r, int c) next = (current.p.r + dir.dr, current.p.c + dir.dc);
-                    if (!input.IsInBounds(next) || input[next.r][next.c] == '#' && current.cheats.Count == 2) {
-                        continue;
-                    }
-
-                    if (current.cheats.Count == 1) {
-                        var newCheats = new List<(int r, int c)>(current.cheats) { current.p };
-                        stack.Push((next, [..current.visited], current.steps + 1, newCheats));
-                        continue;
-                    }
-                    
-                    if (input.IsInBounds(next) && input[next.r][next.c] != '#') {
-                        stack.Push((next, [..current.visited], current.steps + 1, current.cheats));
-                    } else if (input.IsInBounds(next) && input[next.r][next.c] == '#') {
-                        var newCheats = new List<(int r, int c)>(current.cheats) { current.p };
-                        stack.Push((next, [..current.visited], current.steps + 1, newCheats));
-                    }
-                }
+                
             }
-            
-            return saves.Keys.Select(k => saves[k]).ToList();
         }
+
+        foreach (var pair in saves.OrderBy(x => x.Key)) {
+            Console.WriteLine($"{pair.Key}: {pair.Value.Count}");
+        }
+        
+        Console.WriteLine(answer);
 
         List<(int r, int c)> Run((int r, int c) start) {
-            var path = new List<(int r, int c)>();
+            var pathSoFar = new List<(int r, int c)>();
             var visited = new HashSet<(int r, int c)>();
-            var queue = new Queue<(int r, int c)>();
-            queue.Enqueue(start);
-            
-            while (queue.Count != 0) {
-                var current = queue.Dequeue();
-                if (!visited.Add(current)) continue;
-                path.Add(current);
+            // DFS
+            DFS(start);
+            return pathSoFar;
+            void DFS((int r, int c) current) {
+                if (visited.Contains(current)) return;
+                visited.Add(current);
+                pathSoFar.Add(current);
+                if (input[current.r][current.c] == 'E') return;
                 foreach ((int dr, int dc) dir in new[] { (0, 1), (0, -1), (1, 0), (-1, 0) }) {
                     (int r, int c) next = (current.r + dir.dr, current.c + dir.dc);
-                    if (input.IsInBounds(next) && input[next.r][next.c] != '#') {
-                        queue.Enqueue(next);
-                    }
+                    if (!input.IsInBounds(next)|| input[next.r][next.c] == '#') continue;
+                    DFS(next);
                 }
             }
-            return path;
-        }
+        }        
     }
 }
